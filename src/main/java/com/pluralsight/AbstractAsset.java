e2eba569ac0ba10810c514f0529cc23c2c78a6d0
Abigiel <abigailtwg@gmail.com>
2023-11-09 13:25:40 -0500

    commit

--- src/main/java/com/pluralsight/AbstractAsset.java
+++ src/main/java/com/pluralsight/AbstractAsset.java
@@ -1,4 +1,11 @@
 package com.pluralsight;
 
 public class AbstractAsset {
+    public static void main(String[] args) {
+//        Cash saved = new Cash("Saved","2020-20-10", 10000,10000);
+//
+//        System.out.println(saved.getValue());
+
+
+    }
 }
